a549ad1e61c1554a7ac747e758af36d5c0950e2e
Chang Le <le@dbgpu.(none)>
2013-03-19 19:38:05 -0400

    hello

--- atoi.c
+++ atoi.c
@@ -1,3 +1,78 @@
+public class Solution {
+    public int atoi(String str) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+         
+        int i = 0;
+ 
+        if (str.length() == 0) {
+            return 0;
+        }
+        boolean isNeg = false;
+        boolean overFlow = false;
+        char c = '*'; //anything will do
+ 
+        while (i  < str.length()) {
+            c = str.charAt(i);
+            if (Character.isWhitespace(c)) {
+                i++;
+            }
+            else if (c == '+' || c == '-' || Character.isDigit(c)) {
+                break;
+            }
+            else {
+                return 0;
+            }
+        }
+ 
+        if (c == '-') {
+            isNeg = true;
+        }
+ 
+        if (c == '-' || c == '+') {
+            i++;
+        }
+ 
+        int n = 0;
+ 
+        while (i < str.length()) {
+            c = str.charAt(i);
+            if (Character.isDigit(c)) {
+                int x = Character.digit(c, 10);
+                if ((Integer.MAX_VALUE - x)/10 >= n) {
+                    n = 10*n + x;
+                }
+                else {
+                    overFlow = true;
+                    break;
+                }
+                i++;
+            }
+            else {
+                break;
+            }
+        }
+ 
+        if (overFlow) {
+            if (isNeg) {
+                return Integer.MIN_VALUE;
+            }
+            else {
+                return Integer.MAX_VALUE;
+            }
+        }
+        else {
+            if (isNeg) {
+                return -n;
+            }
+            else {
+                return n;
+            }
+        }
+    }
+}
+
+
 #include <stdio.h>
 
 int my_atoi(char *string) {
